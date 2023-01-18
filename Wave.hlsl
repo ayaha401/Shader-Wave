ee0683b53d99e7c377abf0fc890b1645804b7f96
ayaha401 <75297336+ayaha401@users.noreply.github.com>
2023-01-19 02:44:07 +0900

    カッコが片方なかったので修正

--- Wave.hlsl
+++ Wave.hlsl
@@ -8,13 +8,13 @@
 float PI = 3.141592;
 float Wave(int waveMode, float waveSpeed)
 {
-    float speed = _Time.y*waveSpeed)*2.;
+    float speed = _Time.y * waveSpeed;
     float wave = 0.;
-    ifwaveMode == 0)    wave = 1.;
-    ifwaveMode == 1)    wave = (sin(speed*PI)+1.)*.5;
-    ifwaveMode == 2)    wave = frac(speed*.5+.5);
-    ifwaveMode == 3)    wave = abs(2.*frac(speed*.5-.25)-1.);
-    ifwaveMode == 4)    wave = step(.5, frac(speed*.5+.5));
+    if(waveMode == 0)    wave = 1.;
+    if(waveMode == 1)    wave = (sin(speed * PI) + 1.) * .5;
+    if(waveMode == 2)    wave = frac(speed * .5 + .5);
+    if(waveMode == 3)    wave = abs(2. * frac(speed * .5 - .25) - 1.);
+    if(waveMode == 4)    wave = step(.5, frac(speed * .5 + .5));
 
     return wave;
 }
